75237bc0b2ca85eadf3a157835aa16df70d1d636
Ivan Matmati <imatmati@haproxy.com>
2026-03-19 11:55:20 +0100

    TEST: remove test todo tag and task

--- test/integration/base/base_with_map_runtime.go
+++ test/integration/base/base_with_map_runtime.go
@@ -1,5 +1,3 @@
-//go:build test_todo
-
 // Copyright 2019 HAProxy Technologies LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
--- test/integration/hostnames-matchtype/3-gw-exact-route-exact-match-exact-nomatch/suite_test.go
+++ test/integration/hostnames-matchtype/3-gw-exact-route-exact-match-exact-nomatch/suite_test.go
@@ -1,5 +1,3 @@
-//go:build test_todo
-
 // Copyright 2025 HAProxy Technologies LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
