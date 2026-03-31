56c381239b6937cf227ef7a7903e498ac2b1abad
Helene Durand <hdurand@haproxy.com>
2026-03-31 10:42:28 +0200

    CLEANUP/MINOR: Move hostnames match type test 3 in the right place
    No need to have this test in a `to fix` file, it can be in the same file as the other match type tests.

--- test/integration/hostnames-matchtype/hostnames_matchtype_test.go
+++ test/integration/hostnames-matchtype/hostnames_matchtype_test.go
@@ -831,3 +831,35 @@ func (s *HostnamesMatchtypeSuite) Test_24_Empty_Route_Wildcard_Match_Prefix() {
 	mapFilePath = "hug_https_31444"
 	s.ExpectMapContents(mapFilePath, expectedMapsPath)
 }
+
+func (s *HostnamesMatchtypeSuite) Test_3_Exact_Route_Exact_Match_Exact_Nomatch() {
+	fixtureDirPath := utils.GetCRDFixturePath()
+	fixtureDir := "3-gw-exact-route-exact-match-exact-nomatch"
+
+	fixturePath := path.Join(fixtureDirPath, fixtureDir)
+	s.CreateFixtures(fixturePath, nil)
+	defer s.CleanupFixtures(fixturePath, nil)
+
+	// Expected Conditions
+	expectationsPath := path.Join(fixturePath, "expectations")
+	expectedCondPath := path.Join(expectationsPath, "route-conditions.yaml")
+	expectedConditions := s.YamlToRouteConditions(expectedCondPath)
+
+	route := "route-echo-http"
+	// TODO fix
+	// currently conditions are not udpated
+	s.ExpectRouteConditionsUpdated(s.Test().Ctx, s.Test().Namespace, route, expectedConditions)
+
+	// Check AttachedRoutes on Gateway status
+	s.ExpectAttachedRoute(s.Test().Ctx, s.Test().Namespace, "hug-gateway", "http", 0)
+	s.ExpectAttachedRoute(s.Test().Ctx, s.Test().Namespace, "hug-gateway", "https", 0)
+
+	// Check Maps
+	expectedMapsPath := path.Join(expectationsPath, "maps")
+
+	mapFilePath := "hug_http_31081"
+	s.ExpectMapContents(mapFilePath, expectedMapsPath)
+
+	mapFilePath = "hug_https_31444"
+	s.ExpectMapContents(mapFilePath, expectedMapsPath)
+}
