87171a2678a0f9a14c0871b5dd25a5e28d1bc390
Helene Durand <hdurand@haproxy.com>
2026-04-01 09:26:46 +0200

    CLEANUP: remove unused function in conformance test helpers

--- test/conformance/helpers_test.go
+++ test/conformance/helpers_test.go
@@ -18,34 +18,10 @@ package conformance_test
 
 import (
 	"context"
-	"net"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientset "k8s.io/client-go/kubernetes"
 )
-
-// newPortRemappingDialer returns a DialContext function that remaps Gateway API
-// standard ports (80, 443) to the actual NodePort ports HUG listens on.
-// This is needed because the conformance tests construct URLs using the
-// listener port from the Gateway spec (80/443), but HUG serves traffic
-// on NodePort ports (e.g. 31080/31443).
-func newPortRemappingDialer(httpPort, httpsPort string) func(ctx context.Context, network, addr string) (net.Conn, error) {
-	portMap := map[string]string{
-		"80":  httpPort,
-		"443": httpsPort,
-	}
-	return func(ctx context.Context, network, addr string) (net.Conn, error) {
-		host, port, err := net.SplitHostPort(addr)
-		if err != nil {
-			return nil, err
-		}
-		if mappedPort, ok := portMap[port]; ok {
-			addr = net.JoinHostPort(host, mappedPort)
-		}
-		var d net.Dialer
-		return d.DialContext(ctx, network, addr)
-	}
-}
 
 // getNodeIP returns the InternalIP of the first Ready node in the cluster.
 func getNodeIP(ctx context.Context, cs clientset.Interface) string {
